a476704b6d1571e432ad995668fccc1b3783c8a1
Sinjoy Saha <sinjoysaha555@gmail.com>
2018-09-22 19:22:44 +0530

    Translator added

--- morse_code_player.py
+++ morse_code_player.py
@@ -5,24 +5,60 @@ mixer.init()
 dit = mixer.Sound('dit.wav')
 dah = mixer.Sound('dah.wav')
 
-s = "__ ___ ._. ... .   _._. ___ _.. ."
-delay = 0.2
+s = "-- --- .-. ... .   -.-. --- -.. ."
+delay = 1
+charspace = 0.25
+wordspace = 0.7
+wordspace -= charspace
+
+code = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+        'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+        'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+        'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+        '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '.': '.-.-.-', ',': '--..--',
+        '?': '..--..', '=': '-...-'}
+
+
+def translate(inp):
+    s = ""
+    for i in inp:
+        if i == ' ':
+            s = s + '|'
+        else:
+            s = s + code[i] + ' '
+
+    return s
+
+
+inp = input("Enter a string: ")
+print(inp)
+inp = inp.upper()
+print(inp)
+s = ""
+s = translate(inp)
+print(s)
+inp = input("Enter any key to continue: ")
 
 
 def playdit():
     dit.play()
-    time.sleep(delay)
+    time.sleep(delay * 0.18)
 
 
 def playdah():
     dah.play()
-    time.sleep(delay + 0.15)
+    time.sleep(delay * 0.35)
 
 
+tic = time.time()
 for i in s:
     if i == '.':
         playdit()
-    elif i == '_':
+    elif i == '_' or i == '-':
         playdah()
     elif i == ' ':
-        time.sleep(delay * 2)
+        time.sleep(charspace)
+    elif i == '|':
+        time.sleep(wordspace)
+
+print(time.time() - tic)
